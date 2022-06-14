7af181455993beeb314854a6a10930c31d2a8d5c
AnnaWint <anwintermantel@gmail.com>
2022-06-14 16:09:21 +0200

    Verbesserung 10

--- Aufgaben/L10/todo.js
+++ Aufgaben/L10/todo.js
@@ -7,6 +7,8 @@ function newtask() {
     let circle = document.createElement("i");
     circle.classList.add("fa-circle");
     circle.classList.add("fa-thin");
+    let anzahl = document.getElementById("anzahl");
+    anzahl.innerHTML = "anzahl toDos " + (parseInt(anzahl.innerHTML.slice(-1)) + 1);
     circle.addEventListener("click", function () {
         if (circle.hasChildNodes()) {
             circle.innerHTML = "";
@@ -15,15 +17,18 @@ function newtask() {
             let check = document.createElement("i");
             check.classList.add("fa-solid");
             check.classList.add("fa-check");
+            ;
             circle.appendChild(check);
         }
     });
     let inputValue = document.getElementById("input");
     li.innerHTML = inputValue.value;
+    inputValue.value = "";
     document.querySelector("#liste").appendChild(li);
     li.appendChild(circle);
     li.appendChild(trash);
     trash.addEventListener("click", function () {
+        anzahl.innerHTML = "anzahl toDos " + (parseInt(anzahl.innerHTML.slice(-1)) - 1);
         document.querySelector("#liste").removeChild(li);
     });
     inputValue.value;
--- Aufgaben/L10/todo.ts
+++ Aufgaben/L10/todo.ts
@@ -12,28 +12,32 @@ function newtask(): void {
     let circle = document.createElement("i");
     circle.classList.add("fa-circle");
     circle.classList.add("fa-thin");
-        
+    let anzahl = document.getElementById("anzahl");
+    anzahl.innerHTML="anzahl toDos " + (parseInt(anzahl.innerHTML.slice(-1)) + 1)
+      
     
     
     circle.addEventListener("click", function(): void{
         if (circle.hasChildNodes()) {circle.innerHTML = "";
             
         } else { 
-            let check = document.createElement("i");
+            let check: HTMLElement = document.createElement("i");
             check.classList.add("fa-solid");
-            check.classList.add("fa-check");
+            check.classList.add("fa-check");;
             circle.appendChild(check);
 
         
             
         }
-    })
+    });
     let inputValue: HTMLInputElement = (document.getElementById("input") as HTMLInputElement);
     li.innerHTML = inputValue.value;
+    inputValue.value="";
     document.querySelector("#liste").appendChild(li);
     li.appendChild(circle);
     li.appendChild(trash);
     trash.addEventListener("click", function (): void {
+        anzahl.innerHTML="anzahl toDos " + (parseInt(anzahl.innerHTML.slice(-1)) - 1)
         document.querySelector("#liste").removeChild(li);
 
     });
